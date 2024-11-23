d5665615ddda9f33d4dc7bab02ef14997b2da9c9
krd5520 <kdowden621@gmail.com>
2024-11-23 16:14:04 -0500

    more debug

--- R/dp_karwa_vadan_algorithms.R
+++ R/dp_karwa_vadan_algorithms.R
@@ -56,6 +56,7 @@ dp_range<-function(x,sd,epsilon,delta=0,bound.mean,range.prob){
   hist.df=dp_perturbed_hist(hist.df=hist.df,epsilon=epsilon,delta=delta)
   #l-hat in paper. This is the bin that has the highest sanitized proportion
   warning(paste0("Hist Dim",paste(dim(hist.df),collapse=", ")," with colnames",paste0(colnames(hist.df),collapse=", ")))
+  warning(paste0("san.prop head is",paste0(head(hist.df$san.prop),collapse=", ")))
   biggest.san.bin=as.numeric(as.character(hist.df[which.max(hist.df$san.prop),1]))
 
   #get sanitized min and max
