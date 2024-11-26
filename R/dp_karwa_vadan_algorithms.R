2034ade5901e55ebf01c0248bda28496773b29a9
krd5520 <kdowden621@gmail.com>
2024-11-26 13:19:36 -0500

    debug

--- R/dp_karwa_vadan_algorithms.R
+++ R/dp_karwa_vadan_algorithms.R
@@ -61,7 +61,7 @@ dp_range<-function(x,sd,epsilon,delta=0,bound.mean,range.prob){
   biggest.san.bin=as.numeric(as.character(hist.df[which.max(hist.df$san.prop),1]))
   if(length(hist.df[which.max(hist.df$san.prop),1])==0){
     new.sd=dp_estimate_sd(x,epsilon/2,delta/2,c(2^(-15),2^(15)))
-    warning(paste0(paste0(" max san prop is ", max(histdf$san.prop),
+    warning(paste0(paste0(" max san prop is ", max(hist.df$san.prop),
                           " max hist df is ",paste(dim(hist.df[hist.df$san.prop==max(hist.df$san.prop,na.rm=T),]),collapse=", ")),
                    " biggest.san.bin is",biggest.san.bin,
                    " because the bounds is ",bound, " max and min is ",max(x),", ",min(x),
