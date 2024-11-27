0f46b77ecb85528677b4b08f25dd1dd4c07b0187
krd5520 <kdowden621@gmail.com>
2024-11-26 19:21:57 -0500

    debug

--- R/dp_karwa_vadan_algorithms.R
+++ R/dp_karwa_vadan_algorithms.R
@@ -68,8 +68,8 @@ dp_range<-function(x,sd,epsilon,delta=0,bound.mean,range.prob){
     if(length(hist.df[which.max(hist.df$san.prop),1])==0){
       new.sd=dp_estimate_sd(x,epsilon/2,delta/2,c(2^(-15),2^(15)))
       warning(paste(
-        "discretized x is",paste0(length(unique(discretized.x)),collapse=", "),
-        "not na is",paste0(sum(!is.na(x))),
+        "discretized x lenfth is is",paste0(length(unique(discretized.x)),collapse=", "),
+        "colnames",paste0(colnames(hist.df.orig),collapse=", "),
         "orginial hist.df is",paste0(hist.df.orig,collapse=", "),
         "perturbed is ",paste0(hist.df[hist.df$san.prop==max(hist.df$san.prop,na.rm=T),],collapse=", "),
         " max san prop is ", max(hist.df$san.prop),
