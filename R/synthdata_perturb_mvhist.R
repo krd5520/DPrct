a613376c2595ac7eb002296b3535fd8357d593ea
krd5520 <kdowden621@gmail.com>
2024-12-04 22:37:00 -0500

    change treat.colname application

--- R/synthdata_perturb_mvhist.R
+++ R/synthdata_perturb_mvhist.R
@@ -209,7 +209,8 @@ synthdata_perturb_mvhist<-function(data,
       treateffs=synth.data[,colnames(synth.data)%in%conditions]
       treatcol=sapply(seq(1,nrow(treateffs)),function(x)paste0(treateffs[x,],collapse=""))
       warning(paste("length of treat col is",length(treatcol)))
-      synth.data$treatment=treatcol
+      synth.data[,treatment.colname]=treatcol
+      warning("assigned treatment column")
       synth.data$treatment[synth.data=paste0(rep(" ",length(blocks)),collapse="")]="control"
       synth.data$treatment=base::trimws(synth.data$treatment)
 
