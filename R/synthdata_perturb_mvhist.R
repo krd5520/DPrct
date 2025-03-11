e358cb2b71026ddcd75b7633f07549f5e2160a35
krd5520 <kdowden621@gmail.com>
2025-03-11 18:06:47 -0400

    spelling

--- R/synthdata_perturb_mvhist.R
+++ R/synthdata_perturb_mvhist.R
@@ -326,7 +326,7 @@ synthdata_perturb_mvhist<-function(data,
   if(is.null(diagnostic.file)==FALSE){
     CON=file(diagnostic.file,"a")
     writeLines(
-      paste("In MV Histogram: Unrealized rows sampled",num.unreal.sample,"out of",num.unreal.sanprops))
+      paste("In MV Histogram: Unrealized rows sampled",num.unreal.sampled,"out of",num.unreal.sanprops))
     close(CON)
   }
 
